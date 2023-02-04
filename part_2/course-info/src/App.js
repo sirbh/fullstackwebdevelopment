52e9eac39f5ae19f250da77b8c9d7f66a25ea656
saurabh chauhan <sourabh1202@gmail.com>
2023-02-04 17:42:25 +0200

    ex-2.5-done

--- part_2/course-info/src/App.js
+++ part_2/course-info/src/App.js
@@ -1,35 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => {
-      return <Part part={part} key={part.id} />;
-    })}
-  </>
-);
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <h4>
-        total of{" "}
-        {course.parts.reduce((totalExc, part) => {
-          totalExc = totalExc + part.exercises;
-          return totalExc;
-        }, 0)}{" "}
-        exercises
-      </h4>
-    </>
-  );
-};
+import Course from "./component/Course";
 
 const App = () => {
   const courses = [
