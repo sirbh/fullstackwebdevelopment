b06d62dfeb722d93c8dec6be84fadeab1a6eed43
saurabh chauhan <sourabh1202@gmail.com>
2023-02-04 21:07:20 +0200

    ex-2.11-done

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Person = ({ person }) => {
   return (
@@ -53,15 +54,16 @@ const AddPersonForm = ({
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [number, setNumber] = useState("");
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((resposne) => {
+      setPersons(resposne.data);
+    });
+  }, []);
 
   const queryChangeHandler = (event) => {
     setQuery(event.target.value);
